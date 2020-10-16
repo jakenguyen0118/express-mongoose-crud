f271e581684cab19093f7de036c98ebf206cc2f0
Jake Nguyen <jakey@Jakes-Air.fios-router.home>
2020-10-16 15:11:52 -0400

    refractored for async/await

--- student_examples/fruits/controllers/fruits.js
+++ student_examples/fruits/controllers/fruits.js
@@ -7,38 +7,46 @@ const mongoose = require('../db/connection')
 const Fruit = require('../models/Fruit')
 const db = mongoose.connection
 
+// import seed data
+const seedData = require('../db/seedData.json')
+
+// seed..../fruits/seed
+router.get('/seed', async (req, res) => {
+    await Fruit.deleteMany({})
+    const fruits = await Fruit.insertMany(seedData)
+    res.json({status: 200, data: fruits})
+})
+
 // index - returns all
-router.get('/', (req, res) => {
-	Fruit.find({})
-		.then((allFruits) => res.json({ status: 200, data: allFruits }))
-		.catch((err) => console.log(err))
+router.get('/', async (req, res) => {
+    const allFruits = await Fruit.find({})
+    res.json({ status: 200, data: allFruits })
 		// .finally(() => db.close())
 })
 
 // show - returns a single thing
-router.get('/:id', (req, res) => {
-    Fruit.findById(req.params.id)
-        .then((fruit) => res.json({status: 200, data: fruit}))
+router.get('/:id', async (req, res) => {
+    const fruit = await Fruit.findById(req.params.id)
+    res.json({status: 200, data: fruit})
         // .catch(err => console.log(err))
 })
 
 // create - create a single thing
-router.post('/', (req, res) => {
-    const fruit = req.body
-    Fruit.create(fruit)
-        .then((fruit) => res.json({ status: 200, data: fruit}))
+router.post('/', async (req, res) => {
+    const fruit = await Fruit.create(req.body)
+    res.json({ status: 200, data: fruit})
 })
 
 // delete - remove a single thing
-router.delete('/:id', (req, res) => {
-    Fruit.findByIdAndDelete(req.params.id)
-        .then(fruit => res.json({status: 200, msg: 'item deleted', data: fruit}))
+router.delete('/:id', async (req, res) => {
+    const fruit = await Fruit.findByIdAndDelete(req.params.id)
+    res.json({status: 200, msg: 'item deleted', data: fruit})
 })
 
 // put - update a single thing
-router.put('/:id', (req, res) => {
-    Fruit.findByIdAndUpdate(req.params.id, req.body, {new: true})
-        .then(fruit => res.json({status: 200, msg: 'item update', data: fruit,}))
+router.put('/:id', async (req, res) => {
+    const fruit = await Fruit.findByIdAndUpdate(req.params.id, req.body, {new: true})
+    res.json({status: 200, msg: 'item update', data: fruit,})
 })
 
 // export router
